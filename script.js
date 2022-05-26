ffd9a7e0888ab01f46fcd9c230e8e4a6b122488b
Blessedo1004 <93976441+Blessedo1004@users.noreply.github.com>
2022-05-26 12:03:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// function nav1(){
+//     document.getElementById("navitem1").style.backgroundImage="url('Rectangle 11.png')"
+//     document.getElementById("text1").style.color="white"
+//     document.getElementById("navitem2").style.backgroundImage="url()"
+//     document.getElementById("text2").style.color="black"
+//     document.getElementById("navitem3").style.backgroundImage="url()"
+//     document.getElementById("text3").style.color="black"
+//     document.getElementById("navitem4").style.backgroundImage="url()"
+//     document.getElementById("text4").style.color="black"
+//     document.getElementById("navitem5").style.backgroundImage="url()"
+//     document.getElementById("text5").style.color="black"
+
+// };
+// function nav2(){
+//     document.getElementById("navitem2").style.backgroundImage="url('Rectangle 11.png')"
+//     document.getElementById("text2").style.color="white"
+//     document.getElementById("navitem1").style.backgroundImage="url()"
+//     document.getElementById("text1").style.color="black"
+//     document.getElementById("navitem3").style.backgroundImage="url()"
+//     document.getElementById("text3").style.color="black"
+//     document.getElementById("navitem4").style.backgroundImage="url()"
+//     document.getElementById("text4").style.color="black"
+//     document.getElementById("navitem5").style.backgroundImage="url()"
+//     document.getElementById("text5").style.color="black"
+
+// };
+// function nav3(){
+//     document.getElementById("navitem3").style.backgroundImage="url('Rectangle 11.png')"
+//     document.getElementById("text3").style.color="white"
+//     document.getElementById("navitem1").style.backgroundImage="url()"
+//     document.getElementById("text1").style.color="black"
+//     document.getElementById("navitem2").style.backgroundImage="url()"
+//     document.getElementById("text2").style.color="black"
+//     document.getElementById("navitem4").style.backgroundImage="url()"
+//     document.getElementById("text4").style.color="black"
+//     document.getElementById("navitem5").style.backgroundImage="url()"
+//     document.getElementById("text5").style.color="black"
+
+// };
+// function nav4(){
+//     document.getElementById("navitem4").style.backgroundImage="url('Rectangle 11.png')"
+//     document.getElementById("text4").style.color="white"
+//     document.getElementById("navitem1").style.backgroundImage="url()"
+//     document.getElementById("text1").style.color="black"
+//     document.getElementById("navitem3").style.backgroundImage="url()"
+//     document.getElementById("text3").style.color="black"
+//     document.getElementById("navitem2").style.backgroundImage="url()"
+//     document.getElementById("text2").style.color="black"
+//     document.getElementById("navitem5").style.backgroundImage="url()"
+//     document.getElementById("text5").style.color="black"
+// };
+// function nav5(){
+//     document.getElementById("navitem5").style.backgroundImage="url('Rectangle 11.png')"
+//     document.getElementById("text5").style.color="white"
+//     document.getElementById("navitem1").style.backgroundImage="url()"
+//     document.getElementById("text1").style.color="black"
+//     document.getElementById("navitem3").style.backgroundImage="url()"
+//     document.getElementById("text3").style.color="black"
+//     document.getElementById("navitem4").style.backgroundImage="url()"
+//     document.getElementById("text4").style.color="black"
+//     document.getElementById("navitem2").style.backgroundImage="url()"
+//     document.getElementById("text2").style.color="black"
+// };
+
+function GetStarted(){
+    document.getElementById("main-div").style.display="block"
+    document.getElementById("bd").style.backgroundImage="url('bg.png')"
+    const st = document.getElementsByClassName("text")
+    st[0].style.color="white"
+    st[1].style.color="white"
+    st[2].style.color="white"
+    st[3].style.color="white"
+    st[4].style.color="white"
+    document.getElementById  ("bd").style.backgroundRepeat="no repeat"
+    document.getElementById("bd2").style.display="none"
+}
